0aa16c9473e9242315ba46401e43016d2a3f22c2
aaronjan98 <aaronjan98@gmail.com>
2023-04-30 17:03:12 -0700

    update release script with latest contract address

--- scripts/withdrawFunds.js
+++ scripts/withdrawFunds.js
@@ -1,13 +1,13 @@
 require('dotenv').config()
 const Web3 = require('web3')
 const HDWalletProvider = require('@truffle/hdwallet-provider')
-const abi = require('../../provenace-ai-frontend/src/abis/ProvNFT.json')
+const abi = require('../../provenance-ai-frontend/src/abis/ProvNFT.json')
 const providerUrl = `https://goerli.infura.io/v3/${process.env.INFURA_PROJECT_SECRET}`
 const web3 = new Web3(new HDWalletProvider(process.env.MNEMONIC, providerUrl))
 
 const payeeAddress = process.argv[2]
 
-const contractAddress = '0xd2f934b50200b058be56f64c6046cf4f2b6f6e6b'
+const contractAddress = '0xbffd0fbe3374eB25A25A8ceAf8AE8F231b46b89a'
 const contract = new web3.eth.Contract(abi, contractAddress)
 
 async function withdrawFunds() {
@@ -15,8 +15,15 @@ async function withdrawFunds() {
     from: payeeAddress,
     gasPrice: web3.utils.toWei('30', 'gwei'),
   })
+  const weiAmount = tx.events.PaymentReleased.returnValues.amount
+  const ethAmount = web3.utils.fromWei(weiAmount, 'ether')
 
+  console.log('amount released: ', ethAmount, ' eth')
   console.log('Transaction hash:', tx.transactionHash)
+  process.exit()
 }
 
-withdrawFunds()
+withdrawFunds().catch(error => {
+  console.error(error)
+  process.exit(1)
+})
